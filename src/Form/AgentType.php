8a29ab49e66dda3963906c3473801b1ae2af0336
ItSemih <semihbasak25@gmail.com>
2021-11-14 17:23:17 +0100

    feature: refactorying form MissionType.php

--- src/Form/AgentType.php
+++ src/Form/AgentType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\{DateType, SubmitType, TextareaTy
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 class AgentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
