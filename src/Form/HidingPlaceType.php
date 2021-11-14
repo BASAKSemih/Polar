e9a3f2af96e263e3e237a2e677b2073007bbc872
ItSemih <semihbasak25@gmail.com>
2021-11-14 17:21:57 +0100

    feature: refactorying form

--- src/Form/HidingPlaceType.php
+++ src/Form/HidingPlaceType.php
@@ -4,9 +4,7 @@ namespace App\Form;
 
 use App\Entity\HidingPlace;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\{SubmitType, TextareaType, TextType};
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,33 +17,27 @@ class HidingPlaceType extends AbstractType
                 'label' => false,
                 'attr' => [
                     'class' => 'form-control'
-                ]
-            ])
+                ]])
             ->add('city', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'class' => 'form-control'
-                ]
-            ])
+                ]])
             ->add('postalCode', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'class' => 'form-control'
-                ]
-            ])
+                ]])
             ->add('type', TextType::class, [
                 'label' => false,
                 'attr' => [
                     'class' => 'form-control'
-                ]
-            ])
+                ]])
             ->add('submit', SubmitType::class, [
                 'label' => false,
                 'attr' => [
                     'class' => 'btn btn-lg btn-primary mt-2',
-                ]
-            ])
-        ;
+                ]]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
